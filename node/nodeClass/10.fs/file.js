52f6044555c75fd25ea771b83c5dc84850edac7b
yuzhoufen <824132231@qq.com>
2020-02-25 16:45:51 +0800

    update：前端工具 及文本编码

--- node/nodeClass/10.fs/file.js
+++ node/nodeClass/10.fs/file.js
@@ -2,7 +2,7 @@ var fs = require("fs");
 //异步读取
 fs.readFile('input.txt', function(err,data) {
   if(err){
-    retur console.error(err)
+    return console.error(err)
   }
   console.log('异步读取:' + data.toString());
 })
